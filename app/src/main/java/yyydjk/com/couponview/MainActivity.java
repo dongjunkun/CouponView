2224ce0a6fe942f8da73ec7de3c61b10a882f905
dongjunkun <1440418246@qq.com>
2016-09-19 16:35:23 +0800

    demo

--- app/src/main/java/yyydjk/com/couponview/MainActivity.java
+++ app/src/main/java/yyydjk/com/couponview/MainActivity.java
@@ -1,13 +1,65 @@
 package yyydjk.com.couponview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import yyydjk.com.couponview.fragments.CouponGridFragment;
+import yyydjk.com.couponview.fragments.CouponListFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.tab) TabLayout mTab;
+    @Bind(R.id.pager) ViewPager mPager;
+
+    private List<String> titles = Arrays.asList("列表","自定义","卡片");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        mTab.setupWithViewPager(mPager);
     }
+    public class MyPagerAdapter extends FragmentPagerAdapter{
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new CouponListFragment();
+                case 1:
+                    return new CouponGridFragment();
+                default:
+                    return new CouponListFragment();
+            }
+
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return titles.size();
+        }
+    }
+
+
 }
---
+++ app/src/main/java/yyydjk/com/couponview/fragments/CouponGridFragment.java
@@ -0,0 +1,86 @@
+package yyydjk.com.couponview.fragments;
+
+
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zhy.adapter.recyclerview.CommonAdapter;
+import com.zhy.adapter.recyclerview.base.ViewHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import yyydjk.com.couponview.R;
+import yyydjk.com.library.CouponView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CouponGridFragment extends Fragment {
+
+
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+
+    private CommonAdapter<Integer> commonAdapter;
+    private List<Integer> list = new ArrayList<>();
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_coupon_list, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        int[] intArray = getResources().getIntArray(R.array.colors);
+        list.clear();
+        for (int anIntArray : intArray) {
+            list.add(anIntArray);
+        }
+        commonAdapter = new CommonAdapter<Integer>(getActivity(), R.layout.item_coupon_gird, list) {
+            @Override
+            protected void convert(ViewHolder holder, Integer s, int position) {
+                CouponView couponView = holder.getView(R.id.couponView);
+                couponView.setBackgroundColor(s);
+                if (position % 3 == 0){
+                    couponView.setTopSemicircle(true);
+                    couponView.setTopDashLine(false);
+                }else {
+                    couponView.setTopSemicircle(false);
+                    couponView.setTopDashLine(true);
+                }
+            }
+        };
+        mRecyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                super.getItemOffsets(outRect, view, parent, state);
+                outRect.set(dp2Px(6), dp2Px(6), dp2Px(6), 0);
+            }
+        });
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),2));
+        mRecyclerView.setAdapter(commonAdapter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    private int dp2Px(float dp) {
+        return (int) (dp * getActivity().getResources().getDisplayMetrics().density + 0.5f);
+    }
+}
